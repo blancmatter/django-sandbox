9ba6706f27f20a8e283ee55279e659102c5c0ac8
Doug Arnold <disrail@gmail.com>
2019-01-09 18:30:35 +0000

    Going home

--- django/polls/tests.py
+++ django/polls/tests.py
@@ -2,5 +2,21 @@
 from __future__ import unicode_literals
 
 from django.test import TestCase
+from django.utils import timezone
+
+from .models import Question
+
 
 # Create your tests here.
+
+class QuestionModelTests(TestCase):
+
+    def test_was_published_recently_with_future_question(self):
+        """
+        was_published_recently() returns False for questions whose pub_date
+        is in the futureself.
+        """
+        time = timezone.now() + datetime.timedelta(days=30)
+        future_question = Question()
+        futurequestion.pub_date = time
+        self.assertIs(future_question.was_published_recently(), False)
